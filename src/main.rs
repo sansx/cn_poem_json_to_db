bd5a7f19bebeab935f4147334f2f4647873f6351
sansx <xiathxth@gmail.com>
2021-03-14 23:18:52 +0800

    save to db

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,15 @@ use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 use std::env;
 use std::error;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader};
 
 pub mod models;
 pub mod schema;
 
-use models::{NewPoem, Poems};
+use models::{NewPoem, Poems, ResPoems};
 use schema::poems;
 
 #[derive(Serialize, Deserialize)]
@@ -33,35 +32,20 @@ pub fn establish_connection() -> PgConnection {
     PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
 }
 
-// pub fn create_post<'a>(conn: &PgConnection, title: &'a str, body: &'a str) -> Post {
-//     use schema::poems;
-
-//     let new_post = NewPoem { title, body };
-
-//     diesel::insert_into(poems::table)
-//         .values(&new_post)
-//         .get_result(conn)
-//         .expect("Error saving new post")
-// }
-
 fn main() -> Result<(), Box<dyn error::Error>> {
-    use schema::poems;
-    let path = "./guwen/test.json";
-
+    let path = "./guwen/guwen0-1000.json";
+    let connection = establish_connection();
     let input = File::open(path)?;
     let buffered = BufReader::new(input);
-    let mut lines = buffered.lines();
-    // lines.next().unwrap()?;
-    let mut test_target = lines.next().unwrap()?;
-    let mut v: NewPoem = serde_json::from_str(test_target.as_str())?;
-    println!("v:{:?}", v);
-
-    let connection = establish_connection();
-    let _ = diesel::insert_into(poems::table)
-        .values(&v)
-        .get_result::<Poems>(&connection)
-        .expect("Error saving new post");
-    // println!("{:?}", title);
+    let lines = buffered.lines();
+    for poem in lines {
+        let res = poem.unwrap();
+        let v: Poems = serde_json::from_str(&res.as_str().replace("type", "poemtype"))?;
+        let _ = diesel::insert_into(poems::table)
+            .values(&NewPoem::from(v))
+            .get_result::<ResPoems>(&connection)
+            .expect("Error saving new post");
+    }
 
     Ok(())
 }
